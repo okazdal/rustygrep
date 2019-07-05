41013b2cb10880266349abe1c741ab649783629f
okazdal <okazdal@sekuritim.com>
2019-07-05 14:30:12 +0300

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+use std::fs;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+//    println!("{:?}", args);
+
+//    let query = &args[1];
+//    let filename = &args[2];
+
+    let config = parse_config(&args);
+
+//    println!("Searching for {}", query);
+//    println!("In file: {} ", filename);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Error reading file");
+
+    println!("Text: \n{}", contents);
+
+
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config{query, filename}
 }
